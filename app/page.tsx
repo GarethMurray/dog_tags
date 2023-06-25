42c1b3800b7021dd114d1dec7ecaeec4b53e0910
Mike Murray <mikem.tba@gmail.com>
2023-06-26 00:13:53 +0200

    feat: 🎨 Update layout and images

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@
 
 import { FormEvent, useState } from "react";
 import Image from "next/image";
+import spikeJpg from "../public/spike.jpeg";
+import mileyJpg from "../public/miley.jpeg";
 
 export default function Home() {
   const [name, setName] = useState("");
@@ -36,49 +38,65 @@ export default function Home() {
   };
 
   return (
-    <main className="flex min-h-screen flex-col items-start p-4 lg:p-40 container gap-4 mx-auto">
-      <Image src="/dog.jpg" alt="A picture of a dog"></Image>
-      <h1 className="font-bold text-4xl">You&apos;ve found our dog!</h1>
-      <p>Thank you so much for taking the time to follow the qr code</p>
-      <p>
-        Please send me an email using this form, I will get back to you
-        urgently!!
-      </p>
-      <form onSubmit={handleSubmit} className="w-full ">
-        <label className="flex flex-col gap-2 w-1/2 mb-4">
-          Your name
-          <input
-            value={name}
-            type="text"
-            className="form-input rounded"
-            onChange={(e) => setName(e.target.value)}
-          />
-        </label>
-        <label className="flex flex-col gap-2 w-1/2 mb-4">
-          Your email
-          <input
-            value={email}
-            type="email"
-            className="form-input rounded"
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </label>
+    <main className="flex min-h-screen flex-col md:flex-row items-start p-4 lg:p-40 container gap-4 mx-auto">
+      <div className="flex flex-col items-start gap-4">
+        <h1 className="font-bold text-4xl">You&apos;ve found our dog!</h1>
+        <p>Thank you so much for taking the time to follow the qr code</p>
+        <p>
+          Please send me an email using this form, I will get back to you
+          urgently!!
+        </p>
+        <form onSubmit={handleSubmit} className="w-full ">
+          <label className="flex flex-col gap-2 lg:w-1/2 mb-4">
+            Your name
+            <input
+              value={name}
+              type="text"
+              className="form-input rounded"
+              onChange={(e) => setName(e.target.value)}
+            />
+          </label>
+          <label className="flex flex-col gap-2 lg:w-1/2 mb-4">
+            Your email
+            <input
+              value={email}
+              type="email"
+              className="form-input rounded"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </label>
 
-        <label className="flex flex-col gap-2 w-full">
-          Your message
-          <textarea
-            value={text}
-            className="form-textarea w-full h-40 rounded"
-            onChange={(e) => setText(e.target.value)}
-          />
-        </label>
-        <button
-          className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          disabled={isDisabled}
-        >
-          Submit
-        </button>
-      </form>
+          <label className="flex flex-col gap-2 w-full">
+            Your message
+            <textarea
+              value={text}
+              className="form-textarea w-full h-40 rounded"
+              onChange={(e) => setText(e.target.value)}
+            />
+          </label>
+          <button
+            className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            disabled={isDisabled}
+          >
+            Submit
+          </button>
+        </form>
+        <h2 className="text-4xl font-bold mt-6">Thank you!!!</h2>
+      </div>
+      <div className="flex flex-col gap-4 w-full md:w-1/3">
+        <Image
+          src={spikeJpg}
+          alt="A picture of a dog"
+          placeholder="blur"
+          className=""
+        />
+        <Image
+          src={mileyJpg}
+          alt="A picture of a dog"
+          placeholder="blur"
+          className=""
+        />
+      </div>
     </main>
   );
 }
